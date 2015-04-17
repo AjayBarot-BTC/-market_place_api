fedafbfba2494f4890f70ccf364fccbf0e3d57f4
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2015-04-17 15:26:50 +0530

    Initial all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketPlaceApi::Application.config.secret_key_base = 'aafafce59e5a65ab064722809704b664e7633d64d5aac65c46499eab7144d5f38d319cf1b1c392e8f919554220ef3010a83cf1b83ce60245a11b6ecd26ec3523'
