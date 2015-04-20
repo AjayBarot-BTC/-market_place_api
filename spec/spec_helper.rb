2956e4bafecf503ad00cdc7775617b6b9fca07a9
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2015-04-20 17:01:14 +0530

    Refactors test headers for each request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,12 @@ RSpec.configure do |config|
 
   config.include Request::JsonHelpers, :type => :controller
 
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
