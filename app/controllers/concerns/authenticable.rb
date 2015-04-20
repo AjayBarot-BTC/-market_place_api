ff0cb4416cb51d0b3e88db24c1d4a30acf22cd12
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2015-04-20 18:21:02 +0530

    Adds authorization for the users controller

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,5 +1,10 @@
 module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+
+    def authenticate_with_token!
+      render json: { errors: "Not authenticated" },
+             status: :unauthorized unless current_user.present?
+    end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -55,6 +55,7 @@ describe Api::V1::UsersController do
     context "when is successfully updated" do
       before(:each) do
         @user = FactoryGirl.create :user
+        request.headers['Authorization'] =  @user.auth_token
         patch :update, { id: @user.id, user: { email: "newmail@example.com" } }
       end
 
@@ -88,6 +89,7 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
+      api_authorization_header @user.auth_token
       delete :destroy, { id: @user.id }
     end
 
